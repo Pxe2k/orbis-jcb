cd23b0947bdfd301a61898b298f5a7d2a6573c35
Pxe2k <roariffic474@gmail.com>
2023-04-19 18:08:08 +0600

    companies fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->belongsTo(Company::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
